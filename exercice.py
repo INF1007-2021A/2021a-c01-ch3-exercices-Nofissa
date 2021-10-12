d33a3c9150df1e3b509a93b29de12e643cb1a3f3
Nofissa <nofissa.khaif@polymtl.ca>
2021-10-12 17:53:25 -0400

    exercices chapitre 3 compris

--- exercice.py
+++ exercice.py
@@ -4,32 +4,33 @@
 
 import math
 
-def square_root(a: float) -> float:
-    return 0.0
+def square_root(a):
+    return math.sqrt(a)
 
 
-def square(a: float) -> float:
-    return 0.0
+def square(a):
+    return a ** 2
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return sum([a, b, c]) / 3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return math.radians(angle_degs + angle_secs / 3600 + angle_mins / 60)
+
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    return [math.degrees(angles_rads), (angle_rads * 60), (angle_rads * 3600)]
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32) / 1.8
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return (temperature * 1.8) + 32
 
 
 def main() -> None:
